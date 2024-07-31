5c41df17202f311e8819d8b7d927ce6603bf1808
BasselAlShaar <basselalshaar11@gmail.com>
2024-07-31 14:00:31 +0300

    Added Logout button in landing page

--- code-editor/src/components/Navbar/index.jsx
+++ code-editor/src/components/Navbar/index.jsx
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="landing-header">
-            <nav className="navbar">
-                <div className="logo">Logo</div>
-                <ul className="nav-links">
-                    <li><Link to={"/"}>Code</Link></li>
-                    <li><Link to={"/chat"}>Chat</Link></li>
-                    <li><Link to={"/"}>Profile</Link></li>
-                    <li><Link to={"/login"} className="login-btn">Login</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("user-token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user-token");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className="landing-header">
+      <nav className="navbar">
+        <div className="logo">Logo</div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Code</Link>
+          </li>
+          <li>
+            <Link to="/chat">Chat</Link>
+          </li>
+          <li>
+            <Link to="/">Profile</Link>
+          </li>
+          {isLoggedIn ? (
+            <li>
+              <button onClick={handleLogout} className="login-btn">
+                Logout
+              </button>
+            </li>
+          ) : (
+            <li>
+              <button className="login-btn" onClick={()=>{navigate("/login");}}>Login</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
 };
+
 export default Navbar;
-
-
-
-
-
-
-
-
-
